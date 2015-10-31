3d5168dcc98d138ac19839e495f91777d767f00b
ghostbody <423093883@qq.com>
2015-10-31 12:50:17 +0800

    modified data structure

--- src/server/models/entry.py
+++ src/server/models/entry.py
@@ -1,14 +1,13 @@
 
 class Entry(object):
     data = {
-        "title" : ""             # entry's title
-        "describe" : ""          # entry's description
-        "content" : ""           # entry's content
-        "praise" : 0             # entry's praise
-        "author" : None          # entry's author
-        "cover" : -1             # the name of the image
-        "create_time" : None     # utc time
-        "update_time" : None     # utc time
-        "comments" : []          # the comments
+        "title" : "",             # entry's title
+        "content" : "",           # entry's content
+        "praise" : 0,             # entry's praise
+        "author" : None,          # entry's author
+        "cover" : -1,             # the name of the image
+        "create_time" : None,     # utc time
+        "update_time" : None,     # utc time
+        "comments" : [],          # the comments
         "isDelete" : False       # if true the user is deleted
     }
